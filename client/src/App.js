1459635d3ed86de98af576559dd5db974e727554
Justin Bass <justinbass6036@gmail.com>
2023-12-12 22:10:12 -0500

    Deploy test

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <h1>Page Count: {count}</h1>
+      <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2023-07/230724-elon-musk-ac-1041p-bc05fb.jpg" height="200" width="200" />
     </div>
   );
 }
