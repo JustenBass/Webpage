f6f3a753e22184447fa38da20741265de3ac2909
Justin Bass <justinbass6036@gmail.com>
2024-02-06 11:15:28 -0500

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
-      <img src="https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/rockcms/2023-07/230724-elon-musk-ac-1041p-bc05fb.jpg" height="200" width="200" />
+      <div className="test">
+        <h1> HELLO </h1>
+        <img src="https://eu-central.storage.cloudconvert.com/tasks/741be321-6d70-411f-998e-d057176109e2/0F6A136A-5F22-4C51-8E4A-E60806F23F14.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20240206%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20240206T160428Z&X-Amz-Expires=86400&X-Amz-Signature=57556b7a45e2a1e91a5035dcc1395498d1c088b2f91cd6011b59dc5a3a6271cf&X-Amz-SignedHeaders=host&response-content-disposition=inline%3B%20filename%3D%220F6A136A-5F22-4C51-8E4A-E60806F23F14.jpg%22&response-content-type=image%2Fjpeg&x-id=GetObject" width="200px" height="200px"/>
+      </div>
     </div>
   );
 }
